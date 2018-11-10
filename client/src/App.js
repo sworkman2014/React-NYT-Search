c6f8d0e2e34cb07191b1ae0c1f08014056980114
Stephanie Workman <stephanieworkman@Stephanies-MacBook-Pro.local>
2018-11-10 12:04:14 -0600

    added switch

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Saved from "./pages/Saved";
 import Footer from "./components/Footer";
@@ -9,9 +9,11 @@ import './App.css';
   const App = () => (
    <Router>
       <div>
+        <Switch>
          <Route exact path="/" component={Home} />
          <Route exact path="/home" component={Home} />
          <Route exact path="/saved" component={Saved} />
+         </Switch>
         <Footer/>
       </div>
    </Router>

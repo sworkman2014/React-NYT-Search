73ba0c183a19d80987495701c562bb7698be8cfc
Stephanie Workman <stephanieworkman@Stephanies-MacBook-Pro.local>
2018-11-10 12:57:02 -0600

    fix

--- server.js
+++ server.js
@@ -1,38 +1,28 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes");
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Middleware is defined here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// Configure body parser for AJAX requests
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
-//Serve Static Assets
-  // app.use(express.static("public"));
-
-  if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-  }
 // Add routes, both API and view
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nytreact");
-var db = mongoose.connection;
-
-// show any mongoose errors
-db.on('error', function(err) {
-  console.log('Mongoose Error: ', err);
-});
-
-// once logged in to the db through mongoose, log a success message
-db.once('open', function() {
-  console.log('Mongoose connection successful.');
-});
+mongoose.connect(
+  process.env.MONGODB_URI ||
+  "mongodb://localhost/nytarticles"
+);
 
 // Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-});
+app.listen(PORT, () =>
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+);

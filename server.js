29339a2eb917faea6ef4a4f95c3f6049f9a45485
Stephanie Workman <stephanieworkman@Stephanies-MacBook-Pro.local>
2018-11-10 12:22:34 -0600

    push

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.use(express.json());
 //Serve Static Assets
   // app.use(express.static("public"));
 
-  app.use(express.static(path.join(__dirname, 'build')));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('/*', function (req, res) {
-     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
    });
 
 
